9f2b69b4ab574e8c008747561b121a23112a537c
SamaulHaque <samaulhaque749@gmail.com>
2022-12-01 05:22:43 +0600

    my orders data load api created

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ async function run() {
         const categoriesCollection = client.db('mobileGadget').collection('categories');
         const allCategoryCollection = client.db('mobileGadget').collection('allCategory');
         const bookingsCollection = client.db('mobileGadget').collection('bookings');
+        const usersCollection = client.db('mobileGadget').collection('users')
+
 
         app.get('/categories', async (req, res) => {
             const query = {};
@@ -34,11 +36,24 @@ async function run() {
             res.send(category_mobile);
         })
 
+        app.get('/bookings', async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/bookings', async(req, res) => {
             const booking = req.body;
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
+
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+          });
 
     }
     finally {
