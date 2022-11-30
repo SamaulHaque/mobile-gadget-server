773e84f95ee63b24f088e3c0665fe513cb1b03b0
SamaulHaque <samaulhaque749@gmail.com>
2022-11-30 22:52:09 +0600

    create api for brand categoris data load from db

--- index.js
+++ index.js
@@ -14,38 +14,35 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pavt7kq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        const categoriesCollection = client.db('mobileGadget').collection('categories')
+async function run() {
+    try {
+        const categoriesCollection = client.db('mobileGadget').collection('categories');
+        const allCategoryCollection = client.db('mobileGadget').collection('allCategory');
 
-        app.get('/categories', async(req, res)=> {
+        app.get('/categories', async (req, res) => {
             const query = {};
             const categories = await categoriesCollection.find(query).toArray();
             res.send(categories);
         })
+
+        app.get('/category-mobile/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {};
+            const allCategory = await allCategoryCollection.find(query).toArray();
+            const category_mobile = allCategory.filter(mobile => mobile.category_id === id)
+            res.send(category_mobile);
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.log)
 
 
-// const categories = require('./data/categories.json')
-// const allCategory = require('./data/allCategory.json');
-// const { query } = require('express');
 
-// app.get('/categories', (req, res) => {
-//     res.send(categories)
-// })
-
-app.get('/category-mobile/:id', (req, res) => {
-    const id = req.params.id;
-    const category_mobile = allCategory.filter(mobile => mobile.category_id === id)
-    res.send(category_mobile);
-})
-
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     res.send('Mobile gadget server is running');
 })
 
