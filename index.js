3d2f814fc21169c411989e903c01ba4b3fe7c51d
SamaulHaque <samaulhaque749@gmail.com>
2022-12-02 01:25:28 +0600

    seller delete and buyer delete api created

--- index.js
+++ index.js
@@ -124,13 +124,61 @@ async function run() {
             res.send(seller);
           }) 
 
-
+          //admin api
           app.get('/users/admin/:email', async(req, res) => {
             const email = req.params.email;
             const query = {email}
             const user = await usersCollection.findOne(query);
             res.send({isAdmin: user?.role === 'admin'});
           })
+
+          //seller api
+          app.get('/users/seller/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email}
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin: user?.accountType === 'seller'});
+          })
+
+
+          app.delete('/seller/:id',  async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const result = await usersCollection.deleteOne(filter);
+            res.send(result);
+          })
+
+          app.delete('/buyer/:id',  async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const result = await usersCollection.deleteOne(filter);
+            res.send(result);
+          })
+
+
+
+
+          app.put('/users/seller/:id', verifyJWT,  async(req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = {email: decodedEmail};
+            const user = await usersCollection.findOne(query)
+
+            if(user?.accountType !== 'seller'){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)}
+            const options = {upsert: true};
+            const updatedDoc = {
+                $set: {
+                    accountType: 'seller'
+                 }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
+          })
+
 
         app.post('/users', async(req, res) => {
             const user = req.body;
